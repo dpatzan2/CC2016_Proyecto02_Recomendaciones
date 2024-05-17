b7d362b96f5a0181d6d477abd888a591929cc0dc
Diego Patzan <diegopatzan24@gmail.com>
2024-05-16 21:05:21 -0600

    cleaning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,13 @@
 'use client';
 
-import { getAllMovies } from "@/controllers/movies/movies.controller";
-import { User } from "@/types/types";
-import { useEffect, useState } from "react";
 
 
 
 export default function Home() {
 
-  const [users, setUsers] = useState<User[]>([]);
-
-  useEffect(() => {
-    async function fetchData() {
-      const userData = await getAllMovies();
-      console.log(userData)
-      setUsers(userData);
-    }
-    fetchData();
-    console.log(users)
-  }, []);
   return (
     <div>
-      <h1>Users</h1>
-      <ul>
-        {users.map(user => (
-          <li key={user.id}>{user.title}</li>
-        ))}
-      </ul>
+     
     </div>
   );
 }
